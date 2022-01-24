8b9d81f0796207fae66070dd6e0866588cbb3c9f
Erik Stolpe <erik.stolpe98@gmail.com>
2022-01-24 16:59:04 +0100

    Add working prototype

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 // set the dimensions and margins of the graph
-const margin = {top: 80, right: 25, bottom: 30, left: 40},
-  width = 800 - margin.left - margin.right,
-  height = 800 - margin.top - margin.bottom;
+const margin = {top: 80, right: 25, bottom: 30, left: 150},
+  width = 1800 - margin.left - margin.right,
+  height = 900 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#my_dataviz")
@@ -13,7 +13,7 @@ const svg = d3.select("#my_dataviz")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Read the data
-d3.csv("https://github.com/tayloh/tayloh.github.io/blob/main/formatted.csv").then(function(data) {
+d3.csv("https://raw.githubusercontent.com/tayloh/tayloh.github.io/main/formatted.csv").then(function(data) {
 
   // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
   const myGroups = Array.from(new Set(data.map(d => d.group)))
@@ -22,7 +22,7 @@ d3.csv("https://github.com/tayloh/tayloh.github.io/blob/main/formatted.csv").the
   // Build X scales and axis:
   const x = d3.scaleBand()
     .range([ 0, width ])
-    .domain(myGroups)
+    .domain(myVars)
     .padding(0.05);
   svg.append("g")
     .style("font-size", 15)
@@ -33,8 +33,8 @@ d3.csv("https://github.com/tayloh/tayloh.github.io/blob/main/formatted.csv").the
   // Build Y scales and axis:
   const y = d3.scaleBand()
     .range([ height, 0 ])
-    .domain(myVars)
-    .padding(0.05);
+    .domain(myGroups)
+    .padding(0.1);
   svg.append("g")
     .style("font-size", 15)
     .call(d3.axisLeft(y).tickSize(0))
@@ -42,8 +42,8 @@ d3.csv("https://github.com/tayloh/tayloh.github.io/blob/main/formatted.csv").the
 
   // Build color scale
   const myColor = d3.scaleSequential()
-    .interpolator(d3.interpolateInferno)
-    .domain([1,100])
+    .interpolator(d3.interpolateOrRd)
+    .domain([1,10])
 
   // create a tooltip
   const tooltip = d3.select("#my_dataviz")
@@ -61,14 +61,16 @@ d3.csv("https://github.com/tayloh/tayloh.github.io/blob/main/formatted.csv").the
     tooltip
       .style("opacity", 1)
     d3.select(this)
-      .style("stroke", "black")
+      .style("stroke", "white")
       .style("opacity", 1)
   }
   const mousemove = function(event,d) {
     tooltip
-      .html("The exact value of<br>this cell is: " + d.value)
+      .html("" + d.group + " - " + d.variable + ": " + d.value)
       .style("left", (event.x)/2 + "px")
       .style("top", (event.y)/2 + "px")
+      .style("background-color", "#151515")
+      .style("color", "#ddd")
   }
   const mouseleave = function(event,d) {
     tooltip
@@ -80,10 +82,10 @@ d3.csv("https://github.com/tayloh/tayloh.github.io/blob/main/formatted.csv").the
 
   // add the squares
   svg.selectAll()
-    .data(data, function(d) {return d.group+':'+d.variable;})
+    .data(data, function(d) {return d.variable+':'+d.group;})
     .join("rect")
-      .attr("x", function(d) { return x(d.group) })
-      .attr("y", function(d) { return y(d.variable) })
+      .attr("x", function(d) { return x(d.variable) })
+      .attr("y", function(d) { return y(d.group) })
       .attr("rx", 4)
       .attr("ry", 4)
       .attr("width", x.bandwidth() )
@@ -96,22 +98,4 @@ d3.csv("https://github.com/tayloh/tayloh.github.io/blob/main/formatted.csv").the
     .on("mousemove", mousemove)
     .on("mouseleave", mouseleave)
 })
-
-// Add title to graph
-// svg.append("text")
-//         .attr("x", 0)
-//         .attr("y", -50)
-//         .attr("text-anchor", "left")
-//         .style("font-size", "22px")
-//         .text("A d3.js heatmap");
-
-// Add subtitle to graph
-// svg.append("text")
-//         .attr("x", 0)
-//         .attr("y", -20)
-//         .attr("text-anchor", "left")
-//         .style("font-size", "14px")
-//         .style("fill", "grey")
-//         .style("max-width", 400)
-//         .text("A short description of the take-away message of this chart.");
 
